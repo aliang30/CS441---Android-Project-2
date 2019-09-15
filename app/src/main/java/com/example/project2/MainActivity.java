013aa58126017df80a290fb44caaa3c09db541c3
aliang30 <aliang30@binghamton>
2019-09-15 03:42:49 -0400

    Changed layout of the screen + added buttons

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,23 +1,70 @@
 package com.example.project2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
 
-    private PaintView paintView;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button b_red, b_blue, b_green;
+    Button b_reset, b_dotSizePlus, b_dotSizeMinus;
+    TextView tv_dotSize;
+    private static final int DOT_SIZE_INCREMENT = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        paintView = (PaintView) findViewById(R.id.canvas);
+
+        b_red = (Button)findViewById(R.id.red_b);
+        b_blue = (Button)findViewById(R.id.blue_b);
+        b_green = (Button)findViewById(R.id.green_b);
+        b_reset = (Button)findViewById(R.id.reset_b);
+        b_dotSizePlus = (Button)findViewById(R.id.plus_b);
+        b_dotSizeMinus = (Button)findViewById(R.id.minus_b);
+
+        b_red.setOnClickListener(this);
+        b_blue.setOnClickListener(this);
+        b_green.setOnClickListener(this);
+        b_reset.setOnClickListener(this);
+        b_dotSizePlus.setOnClickListener(this);
+        b_dotSizeMinus.setOnClickListener(this);
+
+        tv_dotSize = (TextView)findViewById(R.id.dotSize_tv);
     }
 
-    public void clearCanvas(View v) {
-        paintView.clearCanvas();
+
+    @Override
+    public void onClick(View view) {
+        Button _b = (Button)findViewById(view.getId());
+
+        switch (view.getId()) {
+            case R.id.red_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+            case R.id.blue_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+            case R.id.green_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+            case R.id.reset_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+            case R.id.plus_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+            case R.id.minus_b:
+                Log.d("Button Pressed: ", _b.getText() + "");
+                break;
+        }
     }
 }
